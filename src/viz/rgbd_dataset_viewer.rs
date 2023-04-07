da8df6abb08044b432f454127728cd7d0df083ff
Otavio Gomes <otavio.b.gomes@gmail.com>
2023-04-07 16:22:50 -0300

    Confirm thats camera pose are camera to world transforms

--- src/viz/rgbd_dataset_viewer.rs
+++ src/viz/rgbd_dataset_viewer.rs
@@ -1,44 +1,59 @@
-use std::{cell::RefCell, rc::Rc};
-
 use crate::{
-    io::dataset::RgbdDataset, pointcloud::PointCloud, range_image::RangeImage,
+    io::dataset::RgbdDataset, range_image::RangeImage, trajectory::Trajectory,
 };
 
 use super::{
-    node::{IntoVulkanWorldSpace, MakeNode},
+    node::{IntoVulkanWorldSpace, MakeNode, NodeRef, node_ref, Node},
     scene::Scene,
     Manager, Window,
 };
 
 pub struct RgbdDatasetViewer {
     pub dataset: Box<dyn RgbdDataset>,
+    pub trajectory: Trajectory,
 }
 
 impl RgbdDatasetViewer {
     pub fn new(dataset: Box<dyn RgbdDataset>) -> Self {
-        Self { dataset }
+        let trajectory = dataset.trajectory().unwrap();
+        Self { 
+            dataset,
+            trajectory
+        }
+    }
+
+    pub fn with_trajectory(mut self, trajectory: Trajectory) -> Self {
+        self.trajectory = trajectory;
+        self
     }
 
     pub fn run(&self) {
         let mut manager = Manager::default();
+
+        let scene = self.make_node(&mut manager);
+        let mut window = Window::create(&mut manager, scene);
+        window.show();
+    }
+}
+
+impl MakeNode for RgbdDatasetViewer {
+    type Node = Scene;
+    fn make_node(&self, manager: &mut Manager) -> NodeRef<dyn Node> {
         let mut scene = Scene::default();
-        let trajectory = self.dataset.trajectory().unwrap().first_frame_at_origin();
 
         for i in 0..self.dataset.len() {
             let rgbd_frame = self.dataset.get(i as usize).unwrap();
             let mut ri = RangeImage::from_rgbd_frame(&rgbd_frame);
             ri.compute_normals();
-            let pcl: PointCloud = PointCloud::from(&ri);
-            let node = pcl.make_node(&mut manager);
+            let node = ri.make_node(manager);
 
-            let transform = trajectory[i as usize].clone();
+            let transform = self.trajectory[i as usize].clone();
             node.borrow_mut().properties_mut().transformation =
                 transform.into_vulkan_coordinate_system();
 
             scene.add(node.clone());
         }
 
-        let mut window = Window::create(&mut manager, Rc::new(RefCell::new(scene)));
-        window.show();
+        node_ref(scene)
     }
 }
